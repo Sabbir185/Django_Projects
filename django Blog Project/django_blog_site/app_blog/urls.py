8f340162e6f3fae8cb3b8916c2e947a10fe6164b
sabbir185 <sabbir.cse@yahoo.com>
2020-10-17 22:19:00 +0600

    added some feature and final v2.0

--- django Blog Project/django_blog_site/app_blog/urls.py
+++ django Blog Project/django_blog_site/app_blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('', views.BlogList.as_view(), name='blog_list'),
     path('create-blog/', views.CreateBlog.as_view(), name='create_blog'),
     path('details/<str:slug>/' , views.blogDetails, name='blog_details'),
+    path('likes/<str:slug>/', views.like_post, name='like_post'),
+    path('unlikes/<pk>/', views.unlike_post, name='unlike_post'),
+    path('MyBlog/', views.MyBlog.as_view(), name='my_blog'),
+    path('Edit-Blog/<pk>', views.EditBlog.as_view(), name='edit_blog'),
 ]
--- django Blog Project/django_blog_site/app_blog/views.py
+++ django Blog Project/django_blog_site/app_blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 import uuid
 from .forms import CommentForm
+
 # Create your views here.
 
 
@@ -32,6 +33,11 @@ class BlogList(ListView):
 def blogDetails(request,slug):
     blog = Blog.objects.get(slug=slug)
     comment_form = CommentForm()
+    already_like = Likes.objects.filter(blog=blog,user=request.user)
+    if already_like:
+        like = True
+    else:
+        like = False
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -40,4 +46,38 @@ def blogDetails(request,slug):
             comment.blog = blog
             comment.save()
             return HttpResponseRedirect(reverse('app_blog:blog_details' , kwargs={'slug':slug}))
-    return render(request,'app_blog/blog_details.html',context={'blog':blog, 'comment_form':comment_form })
+    return render(request,'app_blog/blog_details.html',context={'blog':blog, 'comment_form':comment_form , 'like':like , }) 
+
+
+@login_required
+def like_post(request,slug):
+    blog = Blog.objects.get(slug=slug)
+    user = request.user
+    already_like = Likes.objects.filter(blog=blog , user=user)
+    if not already_like:
+        liked_post = Likes(blog=blog , user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('app_blog:blog_details', kwargs={'slug':slug}))
+
+
+@login_required
+def unlike_post(request,pk): # slug or pk/id my wish
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_like = Likes.objects.filter(blog=blog,user=user)
+    already_like.delete()
+    return HttpResponseRedirect(reverse('app_blog:blog_details', kwargs={'slug':blog.slug}))
+
+
+class MyBlog(LoginRequiredMixin,TemplateView):
+    template_name = 'app_blog/my_blog.html'
+
+
+
+class EditBlog(LoginRequiredMixin,UpdateView):
+    model = Blog
+    fields = ('blog_title','blog_content','blog_image')
+    template_name = 'app_blog/edit_blog.html'
+
+    def get_success_url(self,**kwargs):
+        return reverse_lazy('app_blog:blog_details' , kwargs={'slug':self.object.slug})
